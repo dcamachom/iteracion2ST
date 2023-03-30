665fdcd3c4e264113e3851814ffafa5d56e93fb3
ing-v <iv.barbosa@uniandes.edu.co>
2023-03-30 12:53:08 -0500

    cambios

--- Iteracion2/src/Persistencia/SQLApartamento.java
+++ Iteracion2/src/Persistencia/SQLApartamento.java
@@ -1,7 +1,11 @@
 package Persistencia;
 
+import java.util.List;
+
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
+
+import negocio.Apartamento;
 
 
 public class SQLApartamento {
@@ -15,11 +19,31 @@ public class SQLApartamento {
 		this.pa=pa;
 	}
 	
-	public long adicionarApartamento (PersistenceManager pm, long idBar, String nombre, String ciudad, String presupuesto, int sedes) 
+	public long adicionarApartamento (PersistenceManager pm, long idApartamento, boolean amoblado, int cantHabitaciones) 
 	{
-        Query q = pm.newQuery(SQL, "INSERT INTO " + pa.darTablaApartamento () + "(id, nombre, ciudad, presupuesto, cantsedes) values (?, ?, ?, ?, ?)");
-        q.setParameters(idBar, nombre, ciudad, presupuesto, sedes);
+        Query q = pm.newQuery(SQL, "INSERT INTO " + pa.darTablaApartamento () + "(id, amoblado, cantHabitaciones) values (?, ?, ?)");
+        q.setParameters(idApartamento, amoblado, cantHabitaciones);
         return (long) q.executeUnique();
+	}
+	
+	public long eliminarApartamento (PersistenceManager pm, long idApartamento)
+	{
+        Query q = pm.newQuery(SQL, "DELETE FROM " + pa.darTablaApartamento () + " WHERE id = ?");
+        q.setParameters(idApartamento);
+        return (long) q.executeUnique();
+	}
+	public List<Apartamento> darApartamentos (PersistenceManager pm)
+	{
+		Query q = pm.newQuery(SQL, "SELECT * FROM " + pa.darTablaApartamento ());
+		q.setResultClass(Apartamento.class);
+		return (List<Apartamento>) q.executeList();
+	}
+	public Apartamento darApartamentoPorId (PersistenceManager pm, long idApartamento) 
+	{
+		Query q = pm.newQuery(SQL, "SELECT * FROM " + pa.darTablaApartamento () + " WHERE id = ?");
+		q.setResultClass(Apartamento.class);
+		q.setParameters(idApartamento);
+		return (Apartamento) q.executeUnique();
 	}
 
 }
