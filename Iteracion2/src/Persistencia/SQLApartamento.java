413ca9733841c6682d086414db8a864cce4c9e41
ing-v <iv.barbosa@uniandes.edu.co>
2023-03-30 12:49:05 -0500

    sqlApartamento

--- Iteracion2/src/Persistencia/SQLApartamento.java
+++ Iteracion2/src/Persistencia/SQLApartamento.java
@@ -6,5 +6,20 @@ import javax.jdo.Query;
 
 public class SQLApartamento {
 	
+	private final static String SQL = PersistenciaAlohAndes.SQL;
 	
+	private PersistenciaAlohAndes pa;
+	
+	public SQLApartamento(PersistenciaAlohAndes pa)
+	{
+		this.pa=pa;
+	}
+	
+	public long adicionarApartamento (PersistenceManager pm, long idBar, String nombre, String ciudad, String presupuesto, int sedes) 
+	{
+        Query q = pm.newQuery(SQL, "INSERT INTO " + pa.darTablaApartamento () + "(id, nombre, ciudad, presupuesto, cantsedes) values (?, ?, ?, ?, ?)");
+        q.setParameters(idBar, nombre, ciudad, presupuesto, sedes);
+        return (long) q.executeUnique();
+	}
+
 }
